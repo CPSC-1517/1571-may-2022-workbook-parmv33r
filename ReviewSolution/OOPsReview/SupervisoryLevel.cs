68827fad9863b3b217ce3395ed2e9b3761823d7a
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-05-12 17:10:32 -0600

    SupervisoryLevel Enum created

---
+++ ReviewSolution/OOPsReview/SupervisoryLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOPsReview.Data
+{
+	public enum SupervisoryLevel
+	{
+        Entry,              //0
+        TeamMember,         //1
+        TeamLeader,         //2
+        Supervisor,         //3
+        DepartmentHead,     //4
+        Owner               //5
+    }
+}
+

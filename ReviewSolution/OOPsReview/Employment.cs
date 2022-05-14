44d26f8c07a17f6f9f5f8dbc1efa740238fa37e2
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-05-13 18:53:21 -0600

    Employment class updated.

--- ReviewSolution/OOPsReview/Employment.cs
+++ ReviewSolution/OOPsReview/Employment.cs
@@ -24,6 +24,9 @@ namespace OOPsReview.Data
         //  are storage areas in your class
         //  these are treated as variables
         //  these may be public, private, public readonly
+        private string _Title;
+        private double _Years;
+        public int PublicDataField;
 
         //property
         //These are access techniques to retrieve or set data in
@@ -39,11 +42,154 @@ namespace OOPsReview.Data
         //  the set can be either public or  private
         //     public: the user can alter the contents of the data
         //     private: only code within the class can alter the contents of the data
+
+        //Fully implemented porperty
+        // a) a declared storage area (Data feild)
+        // b) a decalred property signature (access returneddatatype propertyname)
+        // c) a coded accessor (get) coding block: public
+        // d) an optional coded mutator (set) codingg block: public or private
+        //      if the set is private the only way to place data into this property is
+        //      via constructor or a behaviour (method)
+
+        // When:
+        // a) if you are storing the associate data in an explicity declared data field
+        // b) if you are doing validation on incoming data
+        // c) creating a property that genereates output from other data sources
+        //      within the class (readonly property); this property would ONLY have a
+        //      accessor (get)
         public string Title
         {
-            get;
-            set;
+            // a property is associated with a single piece of data
+            get
+            {
+                // accessor
+                // the get "coding block" will return the contents of a data field(s)
+                // the return has sytnax of return expression
+                return _Title;
+            }
+
+            set
+            {
+                // mutator
+                // the set "coding block" recieves an incoming value and places it into
+                // the associated data field
+                // during the setting, you might wish to validate the incoming data
+                // during the setting, you might wish to do some type of logical
+                // processing using the data to set another field
+                // the incoming piece of data is referred to using the keyword "value"
+
+                // ensure that the incoming data is not null or empty or just whitespace
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Title is a required piece of data.");
+                }
+
+                // data is considered valid.
+                _Title = value;
+            }
         }
 
+        // auto implemented property
+
+        // these properties differ only in syntax
+        // each property is responsible for a single piece of data
+        // these properties do NOT reference a declared private data member
+        // the system generates an internal storage area of the return data type
+        // the system manages the internal storage for teh accessor and mutator
+        // THERE IS NO ADDITIONAL LOGIN APPLIED TO THE DATA VALUE!!!
+
+        // Using an enum for this field will automatically restrict the data values
+        // this property can contain.
+
+        // syntax:  access rdt  propertyname {get; [private]set;}
+
+
+        public SupervisoryLevel Level { get; set; }
+
+        public double Years
+        {
+            get
+            {
+                return _Years;
+            }
+
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"Years value {value} is invalid. Must be o or greater");
+                }
+
+                _Years = value;
+            }
+        }
+
+        // constructor
+
+        // this is used to initialize the physical object (instance) during its creation
+        // the result of the creation is to ensure that the coder gets an instance in a valid
+        // "Known state"
+        //
+        // if your class definition has no constructor coded, the data members and/or
+        // auto implemented properties are set to the C# default data type value.
+        //
+        // you can code one or more constrcutors in your class definition
+        // IF YOU CODE A CONSTRUCTOR FOR THE CLASS, YOU ARE RESPONSIBLE FOR ALL
+        // CONSTRUCTORS USED BY THE CLASS !!!!!
+        //
+        // generally if you are going to code your own constructor(s) you code two types
+        // Default: this constrcutor does not take in any parameters
+        //          this constructor mimics the default system constructor
+        // Greedy:  this constructor has a list of parameters, one for each property,
+        //          declared for incoming data.
+        //
+        //  (), (a), (b), (c), (d), (a,b), (a,c), (a,d).....2 raise power of 4 = 16 constructors
+        //  (), (a,b,c,d)
+        //
+        // syntax: accessstype  classname([list of parameters]) {constructor code block}
+        //
+        // IMPORTANT: The constructor DOES NOT have a return datatype.
+        //            You DO NOT call constructor directly, it is called using the
+        //              new command (= new classname(.....));
+
+        public Employment()
+        {
+            //constructor body
+            // a) empty: values will be set to c# defaults
+            // b) you COULD assigned literal values to your properties with this constructor
+
+            // the values that you give your class data member/properties CAN be assigned
+            //      directly to a data member.
+            // HOWEVER: if you have validated properties, you SHOULD consider saving your
+            //          data values via property
+
+            // You CAN code your validation logic within your constructors BECAUSE
+            // objects run your constructor when it is created.
+            // Placing your logic in the constructor could be done if you property has
+            //  a private set OR if your public data member is a readonly data member.
+            // Private sets and readonly data members CAN NOT have their data altered directly
+
+            Level = SupervisoryLevel.TeamMember;
+            Title = "Unknown";
+        }
+
+        // Greedy Constructor
+        public Employment(string title, SupervisoryLevel level, double years)
+        {
+            //you COULD your validation in this constructor
+
+            //cnostructor body
+            //  a) a parameter for each property
+            //  b) you COULD could your validation in this contructor.
+            //  c) validation for public readonly data members MUST be done here
+            //  d) validation for properties with a private set MUST be done here
+            //      if not done in the property.
+
+            Title = title;
+            Level = level;
+            Years = years; // eventually the data will be placed in _Years
+
+
+        }
     }
 }

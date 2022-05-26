940a12ee9416bbd897dd3491ec8e15523971bce3
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-05-25 18:49:39 -0600

    Person class created (Compostion explained.)

---
+++ ReviewSolution/OOPsReview/Person.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace OOPsReview.Data
+{
+	public class Person
+	{
+        //Example of a composite class
+        // a composite class uses otehr classes/strucs in its definition
+        // a composite class is recognized with the phrase "has a" class.
+        // this class of Person "has a" resident address
+        // this class has a List<T> where <T> represents a datatype and in this
+        //  class <T> is a collection of Employement.
+
+        // Review video on inheritence and composition
+
+        // each instance of this class will represent an individual
+        // this class will define the following charasterstics of a person
+        //  First Name, Last Name, the current resident address, List of employment positions
+
+
+		private string _FirstName;
+		private string _LastName;
+
+
+
+        public string Firstname
+        {
+            get
+            {
+                return _FirstName;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("First name is required");
+                }
+
+                _FirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _LastName;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Last name is required");
+                }
+
+                _LastName = value;
+            }
+        }
+
+        // Composition actually uses the other struct/class as a property/Feild within
+        //  the definition of the class being specified (created)
+        // in this example address is field that is a data member
+
+        // This field is not a property
+        // The data type is a developer defined datatype (struct)
+
+        public ResidentAddress Address;
+
+        public List<Employment> EmploymentPositions { get; private set; }
+
+        public Person()
+        {
+            // the system will automatically assign default system values to
+            //   our data members according to their datatype.
+            // string -> Null
+            // classes/Objects -> Null
+
+            // firstname and lastname has validations voiding a null value
+
+            Firstname = "Unknown";
+            LastName = "Unknown";
+            // if one tried to reference an instance's data and the instance is
+            // null THEN one would get a exception: null exception
+            // even though you have no instances to store, you will at least have
+            //  someplace to put the data ONCE it is supplied
+
+            EmploymentPositions = new List<Employment>();
+        }
+	}
+}
+

6d5242e6ec5630c8b78b691168e9c36d80e368f9
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-05-27 17:40:13 -0600

    Completed Person class definition

--- ReviewSolution/OOPsReview/Person.cs
+++ ReviewSolution/OOPsReview/Person.cs
@@ -70,23 +70,71 @@ namespace OOPsReview.Data
 
         public List<Employment> EmploymentPositions { get; private set; }
 
-        public Person()
+        //this propery will compile cleanly
+        //this property will return a value IF EMploymentPositions has a instance of List <T>
+        //this property will ABORT IF EmploymentPositions has NOT be set to an instance of List<T>
+
+        public int NumberOfPositions { get { return EmploymentPositions.Count; } }
+
+        //public Person()
+        //{
+        //    // the system will automatically assign default system values to
+        //    //   our data members according to their datatype.
+        //    // string -> Null
+        //    // classes/Objects -> Null
+
+        //    // firstname and lastname has validations voiding a null value
+
+        //    Firstname = "Unknown";
+        //    LastName = "Unknown";
+        //    // if one tried to reference an instance's data and the instance is
+        //    // null THEN one would get a exception: null exception
+        //    // even though you have no instances to store, you will at least have
+        //    //  someplace to put the data ONCE it is supplied
+
+        //    EmploymentPositions = new List<Employment>();
+        //}
+
+        // Option 2
+
+        // Do not code a "Default" Constructor
+        // Code ONLY the "GREEDY" Constructor
+        // If only a constructor exist for the class, the ONLY
+        //  way to possibly create an instance for the class within
+        //  program would be to use the constructor when the class
+        //  instance is created.
+
+        public Person(String firstname, String lastname, ResidentAddress address, List<Employment> employmentpositions)
         {
-            // the system will automatically assign default system values to
-            //   our data members according to their datatype.
-            // string -> Null
-            // classes/Objects -> Null
+            Firstname = firstname;
+            LastName = lastname;
+            Address = address;
+            if (employmentpositions !=null)
+            {
+                EmploymentPositions = employmentpositions;
+            }
 
-            // firstname and lastname has validations voiding a null value
+            else
+            {
+                //Allow a null parameter value and the class to have an empty List<T>
+                EmploymentPositions = new List<Employment>();
+            }
+        }
 
-            Firstname = "Unknown";
-            LastName = "Unknown";
-            // if one tried to reference an instance's data and the instance is
-            // null THEN one would get a exception: null exception
-            // even though you have no instances to store, you will at least have
-            //  someplace to put the data ONCE it is supplied
+        public void ChangeName(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            LastName = lastname;
+        }
 
-            EmploymentPositions = new List<Employment>();
+        public void AddEmployment(Employment employment)
+        {
+            if (employment == null)
+            {
+                throw new ArgumentNullException("You must supply an employment record for it to be added to this person");
+            }
+
+            EmploymentPositions.Add(employment);
         }
 	}
 }

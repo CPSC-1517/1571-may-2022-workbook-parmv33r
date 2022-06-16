9fffa849ff4cec0003b8401d3145baa03e9c6dd7
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-06-16 10:29:10 -0600

    Web application started. Razor Pages introduced.

---
+++ CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public string MyName { get; set; }
+
+    public void OnGet()
+    {
+        Random rnd = new Random();
+        int value = rnd.Next(0, 100); //100 is NOT included.
+        if(value % 2 == 0)
+        {
+            MyName = $"Don ({value}) welcome to the wide wild world of Razor Page";
+        }
+
+        else
+        {
+            MyName = null;
+        }
+
+    }
+}
+

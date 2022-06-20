de1cdadaac91ec3576b9cc13932286c1542f0a4a
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-06-19 19:10:12 -0600

    New Sample page created.

--- CSRazorSolution/WebApp/Pages/Index.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -3,31 +3,48 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages;
 
-public class IndexModel : PageModel
-{
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+//this web page Model class inherits from PageModel
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        //this default page uses a system class called ILogger<T>
+        //this is composition
+        //this is a local field
+        private readonly ILogger<IndexModel> _logger;
 
-    public string MyName { get; set; }
-
-    public void OnGet()
-    {
-        Random rnd = new Random();
-        int value = rnd.Next(0, 100); //100 is NOT included.
-        if(value % 2 == 0)
+        //constructor
+        //this constructor receive an injection of a service
+        //this injection is referred to as Injection Dependency
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            MyName = $"Don ({value}) welcome to the wide wild world of Razor Page";
+            _logger = logger;
         }
 
-        else
-        {
-            MyName = null;
-        }
+        //this is a local property
+        public string MyName { get; set; }
 
+        //this is a class Behaviour (method)
+        //this method, OnGet(), executes for any Get request
+        //this method will be the first method executed when the page is first
+        //  used.
+        //excellent "event" to use to do any initialization to your web page display
+
+        public void OnGet()
+        {
+            //once in the request method, you are in control of what is being
+            //  processed on the web page for the current request
+
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100); //100 is NOT include
+            if (value % 2 == 0)
+            {
+                MyName = $"Don ({value}) welcome to the wide wild world of Razor Pages";
+            }
+            else
+            {
+                MyName = null;
+            }
+
+            //control is returned to the web server
+        }
     }
-}
 

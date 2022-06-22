37b7deb9e6bc46808e155e1ced605214f493549e
Parmveer Baweja <parmveerbaweja@gmail.com>
2022-06-21 19:08:10 -0600

    Form created onPost method created. used BOOTSTRAP

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -7,10 +7,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages.Samples
 {
-	public class BasicDataManagementModel : PageModel
+    public class BasicDataManagementModel : PageModel
     {
+        // BindProperty is an annotation that handles two-way data transfer
+        // two-way? means output to form for display AND input from form for processing
+        [BindProperty]
+        public int Num { get; set; }
+        [BindProperty]
+        public string MassText { get; set; }
+        public string Feedback { get; set; }
         public void OnGet()
         {
+            if (Num == 0)
+            {
+                Feedback = "executing the OnGet method for the Get request";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} display by OnGet";
+            }
+        }
+
+        public void OnPost()
+        {
+            if (Num == 0)
+            {
+                Feedback = "executing the OnPost method for a value of zero";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} (display by OnPost)\n" +
+                $" your mass text is {MassText}";
+            }
         }
     }
 }
